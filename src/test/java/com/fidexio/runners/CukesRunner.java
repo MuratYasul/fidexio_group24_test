30c44e239e71d9bf6a17e2e48c665066f5a0cd39
MuratYasul <113898547+MuratYasul@users.noreply.github.com>
2023-03-31 00:02:39 +0300

    test

--- src/test/java/com/fidexio/runners/CukesRunner.java
+++ src/test/java/com/fidexio/runners/CukesRunner.java
@@ -4,6 +4,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
@@ -14,7 +15,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/fidexio/step_definitions",
-        dryRun = false,
+
+	    dryRun = false,
         tags = "@murat"
 )
 public class CukesRunner {
